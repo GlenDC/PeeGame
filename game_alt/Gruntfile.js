b822faccd3faf5e015b169db2539d0dc74e13615
Maarten Claes <maartencls@gmail.com>
2014-03-29 18:20:11 +0100

    Basic game template

---
+++ game_alt/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    coffee: {
+      compile: {
+        options: {},
+        files: {
+          'dist/game.js': 'src/**/*.coffee'
+        }
+      }
+    },
+    watch: {
+      files: ['**/*.js'],
+      tasks: ['coffee'],
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['coffee', 'watch']);
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
+
